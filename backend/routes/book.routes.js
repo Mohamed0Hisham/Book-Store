dfff66eeb1cc1066bbab20c4a2a272cb9ce16bc2
mohamed HISHAM <hishamsoereign321@gmail.com>
2024-09-18 13:33:52 +0300

    book routes added

--- backend/routes/book.routes.js
+++ backend/routes/book.routes.js
@@ -1,30 +1,13 @@
 import express from "express";
 const router = express.Router();
-import { errorHandler } from "../middleware/errorHandler.js";
-import Book from "../models/book.js";
-router.post("/add", async (req, res, next) => {
-	const { title, category, author, price ,description, image } = req.body;
-	if (!title || !category || !author || !description || !image) {
-		return next(errorHandler(404, "missing input data"));
-	}
-	const book = new Book({
-		title,
-		author,
-		category,
-		price,
-		description,
-		image,
-	});
-	try {
-		const newBook = await book.save();
-		return res.status(201).json({
-			success: true,
-			message: "book added to the database",
-			data: newBook,
-		});
-	} catch (error) {
-		return next(errorHandler(400, error.message));
-	}
-});
+import {
+	addBook,
+	getAllBooks,
+	getBook,
+} from "../controllers/book.controllers.js";
+
+router.post("/add", addBook);
+router.get("/single/:id", getBook);
+router.get("/all", getAllBooks);
 
 export default router;
--- frontend/src/pages/AddBook.jsx
+++ frontend/src/pages/AddBook.jsx
@@ -1,55 +1,72 @@
 import { useRef, useState } from "react";
 import { CLOUD_NAME, UPLOAD_PRESET } from "../config.js";
+import { BsCloudUpload } from "react-icons/bs";
 const AddBook = () => {
 	const [formData, setFormData] = useState({});
 	const [image, setImage] = useState("");
+	const [displayName, setDisplay_name] = useState("");
 	const [loading, setLoading] = useState(false);
 	const imageRef = useRef();
 	const handleInput = (e) => {
 		setFormData({ ...formData, [e.target.id]: e.target.value });
 	};
 
-	const handleImageUpload = (e) => {
-		setLoading(false);
+	const handleImageUpload = async (e) => {
+		setLoading(true);
 		const imageFile = new FormData();
 		imageFile.append("upload_preset", UPLOAD_PRESET);
-		imageFile.append("folder", "Book Store");
+		imageFile.append("asset_folder", "Book Store");
+		imageFile.append("resource_type", "image");
+
+		/**
+		 * we need signed preset to be able to apply transofrmation before upload
+		 */
+		// Add transformations as a JSON string or comma-separated values
+		// imageFile.append(
+		// 	"transformation",
+		// 	"c_fit,w_320,h_480,g_auto,q_auto:good,f_auto"
+		// );
+
 		imageFile.append("file", e.target.files[0]);
 		try {
-			(async () => {
-				//first we upload the images to cloudinary
-				const url = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/upload`;
-				const res = await fetch(url, {
-					method: "POST",
-					body: imageFile,
-				});
-				const { secure_url } = await res.json();
-				console.log(secure_url);
+			//first we upload the images to cloudinary
+			const url = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/upload`;
+			const res = await fetch(url, {
+				method: "POST",
+				body: imageFile,
+			});
+			if (res.ok) {
+				const { secure_url, display_name } = await res.json();
 				setImage(secure_url);
-				setLoading(true);
-			})();
+				setDisplay_name(display_name);
+			}
 		} catch (error) {
 			console.log(error.message);
-			setLoading(true);
+		} finally {
+			setLoading(false);
 		}
 	};
 	const handleSubmit = async (e) => {
 		e.preventDefault();
+		setLoading(true);
+		if (!image) {
+			console.log("no image provided");
+			return;
+		}
+		const data = { ...formData, image: image };
 		try {
 			const serverRes = await fetch("http://localhost:8080/book/add", {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify({
-					...formData,
-					image: image,
-				}),
+				body: JSON.stringify(data),
 			});
-			const success = await serverRes.json();
-			if (success) {
-				console.log("All clear");
+			if (serverRes.ok) {
+				setFormData({});
 			}
 		} catch (error) {
 			console.log(error.message);
+		} finally {
+			setLoading(false);
 		}
 	};
 	return (
@@ -57,8 +74,8 @@ const AddBook = () => {
 			<form
 				encType="multipart/form-data"
 				onSubmit={handleSubmit}
-				className="mt-16 w-[95%] h-[95%] mx-auto">
-				<ul className="w-full h-full flex items-center justify-center flex-col gap-y-10 rounded-lg shadow-md bg-gray-50">
+				className="mt-16 w-[95%] h-[96%] mx-auto">
+				<ul className="w-full h-full flex items-center justify-center flex-col gap-y-6 rounded-lg shadow-md bg-gray-50">
 					<h1 className="text-center font-bold font-serif text-2xl">
 						Fill the following Book data
 					</h1>
@@ -143,44 +160,64 @@ const AddBook = () => {
 						/>
 					</li>
 					<li className="flex flex-col">
-						<label
-							htmlFor="image"
-							className="uppercase pl-1 font-mono">
-							image<span className="text-red-500">*</span>
-						</label>
-						<div
-						onDrop={(e)=>{
-							e.preventDefault();
-							e.dataTransfer.files
-						}}
-							onClick={() => {
-								imageRef.current.click();
-							}}
-							className="sm:w-96 h-48 font-mono hover:bg-gray-400 duration-300 bg-gray-300 p-2 rounded-lg shadow-sm pl-4">
-							<h2>Click to choose an image</h2>
-							<input
-								ref={imageRef}
-								onInput={handleImageUpload}
-								className="hidden sm:w-96 p-2 rounded-lg shadow-sm pl-4"
-								type="file"
-								accept="image/*"
-								name="image"
-								id="image"
-							/>
-						</div>
+						{loading ? (
+							<>
+								<label
+									htmlFor="image"
+									className="uppercase pl-1 font-mono">
+									uploading
+								</label>
+								<BsCloudUpload className="sm:w-96 w-24 h-24 animate-pulse" />
+							</>
+						) : (
+							<>
+								<label
+									htmlFor="image"
+									className="uppercase pl-1 font-mono">
+									image<span className="text-red-500">*</span>
+								</label>
+								<div
+									onDrop={(e) => {
+										e.preventDefault();
+										e.dataTransfer.files;
+									}}
+									onClick={() => {
+										imageRef.current.click();
+									}}
+									className="sm:w-96 h-48 font-mono hover:opacity-60 duration-300 bg-gray-300 rounded-lg shadow-sm">
+									<img
+										src={image ? image : ""}
+										alt={
+											displayName ||
+											"click to choose an image"
+										}
+										className="w-full h-full object-cover rounded-lg"
+									/>
+									<input
+										ref={imageRef}
+										onInput={handleImageUpload}
+										className="hidden sm:w-96 p-2 rounded-lg shadow-sm pl-4"
+										type="file"
+										accept="image/*"
+										name="image"
+										id="image"
+									/>
+								</div>
+							</>
+						)}
 					</li>
 					<li>
 						{loading ? (
 							<button
-								className="p-4 font-light bg-gray-200 rounded-xl hover:bg-gray-300 hover:scale-95 duration-300"
-								type="submit">
-								Add Book
-							</button>
-						) : (
-							<button
 								className="bg-red-300 p-3 rounded-lg"
 								disabled>
 								Fill all data
+							</button>
+						) : (
+							<button
+								className="p-4 font-light bg-gray-200 rounded-xl hover:bg-gray-300 hover:scale-95 duration-300"
+								type="submit">
+								Add Book
 							</button>
 						)}
 					</li>
